344dd9b3cdabc4254d9ea50c12e070d587b1cc83
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2022-02-16 15:54:45 +0530

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public interface Client
+    {
+        void deletecontact(long phno);
+        void addContact(long phno, string name);
+        void createGroup(long phno, string name);
+    }
+}
